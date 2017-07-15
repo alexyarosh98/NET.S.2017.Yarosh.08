68415ab925a22478e8187fe6166a5b06eff7ac72
Alexander <alexyarosh98@gmail.com>
2017-07-15 03:26:13 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Customer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerFormatter
+{
+    public class Customer : IFormattable
+    {
+        public string Name { get; set; }
+        private long phone;
+        public long Phone
+        {
+            get { return phone; }
+            set
+            {
+                if (value.ToString().Length == 11)
+                    phone = value;
+                else throw new ArgumentException($"{nameof(value)} is not allowed to be a phone");
+            }
+        }
+        public decimal Revanue { get; set; }
+        /// <summary>
+        /// Convert an object Customer to a string view
+        /// </summary>
+        /// <returns>string view of an object</returns>
+        public override string ToString()
+        {
+            return this.ToString("G", new MFormatProvider());
+        }
+        /// <summary>
+        /// Convert an object Customer to a string view
+        /// </summary>
+        /// <returns>string view of an object</returns>
+        public string ToString(string format) => ToString(format, CultureInfo.CurrentCulture);
+        /// <summary>
+        /// Convert an object Customer to a string view
+        /// </summary>
+        /// <param name="format">A format string containing formatting specifications</param>
+        /// <param name="fp">An object that supplies format information about the current instance</param>
+        /// <returns>The string representation of the value of arg, formatted as specified by format and formatProvider.</returns>
+        public string ToString(string format, IFormatProvider fp)
+        {
+            if (string.IsNullOrEmpty(format)) format = "G";
+            if (fp == null) fp = CultureInfo.CurrentCulture;
+            string res = string.Empty;
+            switch (format.Length > 1 ? format.Substring(0, 1) : format)
+            {
+                case "G":
+                    res = $"Customer record: {Name}, Phone: {Phone}";
+                    break;
+                case "A":
+                    res = $"Customer record: {Name}, Phone: {Phone}, Revanue: {Revanue}";
+                    break;
+                case "R":
+                    res = string.Format("Customer record: Revanue: {0:C2}", Revanue);
+                    break;
+                default:
+                    throw new FormatException($"The format of '{format}' is invalid.");
+            }
+            return res;
+        }
+
+    }
+}
---
+++ ClassLibrary1/MFormatProvider.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerFormatter
+{
+    public class MFormatProvider : IFormatProvider, ICustomFormatter
+    {/// <summary>
+     /// Returns an object that provides formatting services for the specified type.
+     /// </summary>
+     /// <param name="type">An object that specifies the type of format object to return.</param>
+     /// <returns>An instance of the object specified by formatType, if the IFormatProvider implementation can supply that type of object; otherwise, null.</returns>
+        public object GetFormat(Type type)
+        {
+            return type == typeof(ICustomFormatter) ? this : null;
+        }
+
+        /// <summary>
+        /// Converts the value of a specified object to an equivalent string representation using specified format and culture-specific formatting information.
+        /// </summary>
+        /// <param name="format">A format string containing formatting specifications.</param>
+        /// <param name="obj">An object to format.</param>
+        /// <param name="formatProvider">An object that supplies format information about the current instance.</param>
+        /// <returns>The string representation of the value of arg, formatted as specified by format and formatProvider.</returns>
+        public string Format(string format, object obj, IFormatProvider formatProvider)
+        {
+            if (obj.GetType() != typeof(Customer) || string.IsNullOrEmpty(format))
+            {
+                return HandleOtherFormats(format, formatProvider);
+            }
+
+            string res = string.Empty;
+            if (obj.GetType() == typeof(Customer) && !String.IsNullOrEmpty(format))
+            {
+                Customer customer = (Customer)obj;
+
+                switch (format.ToUpper())
+                {
+                    case "FULLR":
+                        res = string.Format("Customer record: Name: {0}, Phone: {1:+ # (###) ### ####}, Revanue: {2:C}", customer.Name, customer.Phone, customer.Revanue);
+                        break;
+                    case "FULLE":
+                        res = string.Format("Customer record: Name: {0}, Phone: {1:+ # (###) ### ####}, Revanue: {2}", customer.Name, customer.Phone, (customer.Revanue / 2).ToString("C", new CultureInfo("en-US")));
+                        break;
+                    default:
+                        try
+                        {
+                            return HandleOtherFormats(format, obj);
+                        }
+                        catch (FormatException)
+                        {
+                            throw new FormatException($"The format of '{format}' is invalid.");
+                        }
+                }
+            }
+
+            return res;
+        }
+        private static string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            else if (arg != null)
+                return arg.ToString();
+            else
+                return String.Empty;
+        }
+
+    }
+}
